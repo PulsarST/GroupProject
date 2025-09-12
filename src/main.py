a5218744996bbbec06d5921d2ed3780eb6319b0b
PulsarST <tamerlan.zh25@gmail.com>
2025-09-13 00:58:22 +0500

    add black formatter

--- src/main.py
+++ src/main.py
@@ -7,11 +7,15 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="template")
 
+
 @app.get("/{name}")
 async def index(request: Request, name: str) -> _TemplateResponse:
     return templates.TemplateResponse(
-            "index.html", {"request": request, "title": "Home", "name": name})
+        "index.html", {"request": request, "title": "Home", "name": name}
+    )
+
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app="main:app", host="127.0.0.1", port=8000, reload=True)
