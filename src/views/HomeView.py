d95dbb8a725cee5dcca4770808d8f287533ffc2b
PulsarST <tamerlan.zh25@gmail.com>
2025-10-22 09:38:49 +0500

    fixed image on home page

--- src/views/HomeView.py
+++ src/views/HomeView.py
@@ -10,6 +10,7 @@ class HomeContent(ft.Row):
             alignment=ft.MainAxisAlignment.CENTER,
             vertical_alignment=ft.CrossAxisAlignment.START,
         )
+
         self.controls = [
             ft.Column(
                 controls=[
@@ -25,7 +26,11 @@ class HomeContent(ft.Row):
                     ),
                 ]
             ),
-            ft.Image(src=f"choose_us.png", width=1024 / 2, height=1536 / 2),
+            ft.Image(
+                src=f"https://sdmntprwestcentralus.oaiusercontent.com/files/00000000-8184-61fb-a617-4318a023d0cd/raw?se=2025-10-22T05%3A35%3A09Z&sp=r&sv=2024-08-04&sr=b&scid=6d7d842f-e541-43cb-9185-e94712fb30e0&skoid=1e4bb9ed-6bb5-424a-a3aa-79f21566e722&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-10-21T23%3A46%3A50Z&ske=2025-10-22T23%3A46%3A50Z&sks=b&skv=2024-08-04&sig=XMBfZ%2B9q/NPKHZkh7zCVt26TMUZwZiYmHGNYYcjaHnE%3D",
+                width=1024 / 2,
+                height=1536 / 2
+            )
         ]
 
     def on_create_order_click(self, _):
